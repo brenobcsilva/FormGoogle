af31603f47afde32a7ea98fa48854d3e3acf2b71
Breno <breno.csilva@hotmail.com>
2022-10-05 21:47:21 -0300

    Criando class token

--- frontend/exam-generator-jsf/src/app/app-constants.ts
+++ frontend/exam-generator-jsf/src/app/app-constants.ts
@@ -1,7 +1,17 @@
 export class AppConstants {
 
   public static get baseSevidor(): string {
-    return "http"
+    return "http://localhost:8085/"
   }
+
+  public static get baseLogin(): string{
+    return this.baseSevidor + "v1/professor"
+  }
+
+  public static get baseUrl(): string{
+    return this.baseSevidor + "v1/professor"
+  }
+
+
 
 }
